45a08d737970d99353a6b5f64bc51200e0876708
Yuto.K <lazy.dog.triger@gmail.com>
2024-04-03 17:07:36 +0900

    change entering proccess to individual

--- settings.js
+++ settings.js
@@ -1,1 +1,1 @@
-const GAS_URL = "https://script.google.com/macros/s/AKfycbxafVtvcLYU5H23JbGbNTYqg6oPhfIL6IRq7gYu5yo7aov14TnqEUMEpxl6-aZrvUg6Ww/exec"
+const GAS_URL = "https://script.google.com/macros/s/AKfycbxbGf3GPIGF2-MydizWVxZFNV9QswDZ60l4z4PZqSrQJgnFpq_iTcaTF7JEBjIYB0Fjsw/exec"
